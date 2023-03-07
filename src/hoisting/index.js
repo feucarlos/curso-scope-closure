ba0066b2239e0b79993f8ddf50e5cb9a22668b6f
José Carlos Gutiérrez Rojas <feucarlos@gmail.com>
2023-03-06 23:11:32 -0600

    actualizaciones

---
+++ src/hoisting/index.js
@@ -0,0 +1,13 @@
+// var nameOfDog;
+
+// console.log(nameOfDog);
+// var nameOfDog = 'Perro';
+// console.log(nameOfDog);
+
+nameOfDog();
+ 
+function nameOfDog(){
+    console.log(`El mejor amigo del hombre es ${miPerro}`);
+}
+
+var miPerro = 'firubraus';
